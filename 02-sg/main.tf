24ddcb632d622429ef70b7efd50019744b46151d
venkatsurya <venkatsurya1608@gmail.com>
2024-06-18 15:51:14 +0530

    infra

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -224,3 +224,21 @@ resource "aws_security_group_rule" "web_alb_public_https" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.web_alb.sg_id
 }
+
+resource "aws_security_group_rule" "backend_default_vpc" {    
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
+
+resource "aws_security_group_rule" "frontend_default_vpc" {    
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.frontend.sg_id
+}
