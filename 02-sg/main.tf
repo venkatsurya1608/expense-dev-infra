fbb4b96e92e3a84bbb9dfe86534c407ab99d841a
venkatsurya <venkatsurya1608@gmail.com>
2024-06-20 12:25:14 +0530

    some-message

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -70,84 +70,77 @@ module "web_alb" {
   sg_name = "web_alb"
 }
 
-
-# db is accepting connection from  backend 
-resource "aws_security_group_rule" "db_backend" {    # terraform sg rule (db backend connection nunchi accept chesthundi)
+# DB is accepting connections from backend
+resource "aws_security_group_rule" "db_backend" {
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
-  source_security_group_id = module.backend.sg_id   # cidr kadhu source akkada nunchi vasthundi ani (backend)
-  security_group_id = module.db.sg_id
-}
-#  db is accepting connection from  bastion
-resource "aws_security_group_rule" "db_bastion" {    
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id   
-  security_group_id = module.db.sg_id
-}
-# db is accepting connection from  vpn
-resource "aws_security_group_rule" "db_vpn" {    
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id   
+  source_security_group_id = module.backend.sg_id # source is where you are getting traffic from
   security_group_id = module.db.sg_id
 }
 
-#backend is accepting connection from  app_alb 
-resource "aws_security_group_rule" "backend_app_alb" {   # ( backend frontend connection nunchi accept chesthundi)
+resource "aws_security_group_rule" "db_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
+  security_group_id = module.db.sg_id
+}
+
+resource "aws_security_group_rule" "db_vpn" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id # source is where you are getting traffic from
+  security_group_id = module.db.sg_id
+}
+
+resource "aws_security_group_rule" "backend_app_alb" {
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
-  source_security_group_id = module.app_alb.sg_id   # cidr kadhu source akkada nunchi vasthundi ani (frontend)
+  source_security_group_id = module.app_alb.sg_id # source is where you are getting traffic from
   security_group_id = module.backend.sg_id
 }
 
-#  backend is accepting connection from bastion
-resource "aws_security_group_rule" "backend_bastion" {     
+resource "aws_security_group_rule" "backend_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id   
+  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
   security_group_id = module.backend.sg_id
 }
 
-#backend is accepting connection from  vpn_ssh
-resource "aws_security_group_rule" "backend_vpn_ssh" {   
+resource "aws_security_group_rule" "backend_vpn_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id   
+  source_security_group_id = module.vpn.sg_id # source is where you are getting traffic from
   security_group_id = module.backend.sg_id
 }
 
-#backend is accepting connection from  vpn_http
-resource "aws_security_group_rule" "backend_vpn_http" {   
+resource "aws_security_group_rule" "backend_vpn_http" {
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id  
+  source_security_group_id = module.vpn.sg_id # source is where you are getting traffic from
   security_group_id = module.backend.sg_id
 }
 
-#  app_shh is accepting connection from vpn
-resource "aws_security_group_rule" "app_alb_vpn" {     
+resource "aws_security_group_rule" "app_alb_vpn" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id   
+  source_security_group_id = module.vpn.sg_id # source is where you are getting traffic from
   security_group_id = module.app_alb.sg_id
-
 }
 
 resource "aws_security_group_rule" "app_alb_bastion" {
@@ -168,55 +161,43 @@ resource "aws_security_group_rule" "app_alb_frontend" {
   security_group_id = module.app_alb.sg_id
 }
 
-# backend is accepting connection from  public 
-resource "aws_security_group_rule" "frontend_web_alb" {    # (frontend public connection nunchi accept chesthundi)
+resource "aws_security_group_rule" "frontend_web_alb" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  source_security_group_id = module.web_alb.sg_id 
   security_group_id = module.frontend.sg_id
 }
 
-# frontend is accepting connection from  bastion 
-resource "aws_security_group_rule" "frontend_bastion" {    
+resource "aws_security_group_rule" "frontend_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = module.bastion.sg_id
+  source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
   security_group_id = module.frontend.sg_id
-}  
+}
 
-# frontend is accepting connection from  bastion 
-resource "aws_security_group_rule" "frontend_vpn" {    
+resource "aws_security_group_rule" "frontend_vpn" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id
+  source_security_group_id = module.vpn.sg_id # source is where you are getting traffic from
   security_group_id = module.frontend.sg_id
-}  
-#  bastion is accepting connection from  public 
-resource "aws_security_group_rule" "bastion_public" {    
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = module.bastion.sg_id
-}  
+}
 
-resource "aws_security_group_rule" "web_alb_public" {    
+resource "aws_security_group_rule" "web_alb_public" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.web_alb.sg_id
-}  
+}
 
-resource "aws_security_group_rule" "web_alb_public_https" {    
+resource "aws_security_group_rule" "web_alb_public_https" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
@@ -225,7 +206,17 @@ resource "aws_security_group_rule" "web_alb_public_https" {
   security_group_id = module.web_alb.sg_id
 }
 
-resource "aws_security_group_rule" "backend_default_vpc" {    
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
+}
+
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "backend_default_vpc" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -234,7 +225,8 @@ resource "aws_security_group_rule" "backend_default_vpc" {
   security_group_id = module.backend.sg_id
 }
 
-resource "aws_security_group_rule" "frontend_default_vpc" {    
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "frontend_default_vpc" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
@@ -242,3 +234,185 @@ resource "aws_security_group_rule" "frontend_default_vpc" {
   cidr_blocks = ["172.31.0.0/16"]
   security_group_id = module.frontend.sg_id
 }
+
+# not required, we can connect from VPN
+# resource "aws_security_group_rule" "frontend_public" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = module.frontend.sg_id
+# }
+
+# # db is accepting connection from  backend 
+# resource "aws_security_group_rule" "db_backend" {    # terraform sg rule (db backend connection nunchi accept chesthundi)
+#   type              = "ingress"
+#   from_port         = 3306
+#   to_port           = 3306
+#   protocol          = "tcp"
+#   source_security_group_id = module.backend.sg_id   # cidr kadhu source akkada nunchi vasthundi ani (backend)
+#   security_group_id = module.db.sg_id
+# }
+# #  db is accepting connection from  bastion
+# resource "aws_security_group_rule" "db_bastion" {    
+#   type              = "ingress"
+#   from_port         = 3306
+#   to_port           = 3306
+#   protocol          = "tcp"
+#   source_security_group_id = module.bastion.sg_id   
+#   security_group_id = module.db.sg_id
+# }
+# # db is accepting connection from  vpn
+# resource "aws_security_group_rule" "db_vpn" {    
+#   type              = "ingress"
+#   from_port         = 3306
+#   to_port           = 3306
+#   protocol          = "tcp"
+#   source_security_group_id = module.vpn.sg_id   
+#   security_group_id = module.db.sg_id
+# }
+
+# #backend is accepting connection from  app_alb 
+# resource "aws_security_group_rule" "backend_app_alb" {   # ( backend frontend connection nunchi accept chesthundi)
+#   type              = "ingress"
+#   from_port         = 8080
+#   to_port           = 8080
+#   protocol          = "tcp"
+#   source_security_group_id = module.app_alb.sg_id   # cidr kadhu source akkada nunchi vasthundi ani (frontend)
+#   security_group_id = module.backend.sg_id
+# }
+
+# #  backend is accepting connection from bastion
+# resource "aws_security_group_rule" "backend_bastion" {     
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   source_security_group_id = module.bastion.sg_id   
+#   security_group_id = module.backend.sg_id
+# }
+
+# #backend is accepting connection from  vpn_ssh
+# resource "aws_security_group_rule" "backend_vpn_ssh" {   
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   source_security_group_id = module.vpn.sg_id   
+#   security_group_id = module.backend.sg_id
+# }
+
+# #backend is accepting connection from  vpn_http
+# resource "aws_security_group_rule" "backend_vpn_http" {   
+#   type              = "ingress"
+#   from_port         = 8080
+#   to_port           = 8080
+#   protocol          = "tcp"
+#   source_security_group_id = module.vpn.sg_id  
+#   security_group_id = module.backend.sg_id
+# }
+
+# #  app_shh is accepting connection from vpn
+# resource "aws_security_group_rule" "app_alb_vpn" {     
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   source_security_group_id = module.vpn.sg_id   
+#   security_group_id = module.app_alb.sg_id
+
+# }
+
+# resource "aws_security_group_rule" "app_alb_bastion" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   source_security_group_id = module.bastion.sg_id # source is where you are getting traffic from
+#   security_group_id = module.app_alb.sg_id
+# }
+
+# resource "aws_security_group_rule" "app_alb_frontend" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   source_security_group_id = module.frontend.sg_id # source is where you are getting traffic from
+#   security_group_id = module.app_alb.sg_id
+# }
+
+# # backend is accepting connection from  public 
+# resource "aws_security_group_rule" "frontend_web_alb" {    # (frontend public connection nunchi accept chesthundi)
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = module.frontend.sg_id
+# }
+
+# # frontend is accepting connection from  bastion 
+# resource "aws_security_group_rule" "frontend_bastion" {    
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   source_security_group_id = module.bastion.sg_id
+#   security_group_id = module.frontend.sg_id
+# }  
+
+# # frontend is accepting connection from  bastion 
+# resource "aws_security_group_rule" "frontend_vpn" {    
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   source_security_group_id = module.vpn.sg_id
+#   security_group_id = module.frontend.sg_id
+# }  
+# #  bastion is accepting connection from  public 
+# resource "aws_security_group_rule" "bastion_public" {    
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = module.bastion.sg_id
+# }  
+
+# resource "aws_security_group_rule" "web_alb_public" {    
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = module.web_alb.sg_id
+# }  
+
+# resource "aws_security_group_rule" "web_alb_public_https" {    
+#   type              = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#   security_group_id = module.web_alb.sg_id
+# }
+
+# resource "aws_security_group_rule" "backend_default_vpc" {    
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["172.31.0.0/16"]
+#   security_group_id = module.backend.sg_id
+# }
+
+# resource "aws_security_group_rule" "frontend_default_vpc" {    
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks = ["172.31.0.0/16"]
+#   security_group_id = module.frontend.sg_id
+# }
