69f35139352487d2e2218cebea3c163bcac35de8
venkatsurya <venkatsurya1608@gmail.com>
2024-06-20 22:36:42 +0530

    some-message

--- 02-sg-new/provider.tf
+++ 02-sg-new/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
     backend "s3" {    
         bucket = "venkatdevops-remote-state"    
-        key    = "terraform-expense-infra-frontend"  #everytime will change key   
+        key    = "terraform-expense-infra-sg-new"  #everytime will change key   
         region = "us-east-1" 
         dynamodb_table = "venkatdevops-locking" 
     }
